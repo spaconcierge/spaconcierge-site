ebe33ce530da1bd70a84752b65b4cf4a2eba7da8
spaconcierge <matt@getspaconcierge.com>
2025-09-05 14:42:52 -0400

    Update _spa.js

--- netlify/functions/_spa.js
+++ netlify/functions/_spa.js
@@ -1,1 +1,14 @@
+// _spa.js
+let map = {};
+try {
+  map = JSON.parse(process.env.SPA_CONFIG_JSON || '{}');
+} catch {}
 
+function spaForNumber(twilioTo) {
+  // map like { "+14155551234": { name: "Glow Spa" } }
+  return (map[twilioTo] && map[twilioTo].name)
+      || process.env.DEFAULT_SPA_NAME
+      || 'SpaConcierge';
+}
+
+module.exports = { spaForNumber };
